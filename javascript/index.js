10070bd041709d52b3cb5cd97c1bcc780761bdb7
ddffk12 <ddffk12@naver.com>
2022-10-28 16:57:27 +0900

    up

---
+++ javascript/index.js
@@ -0,0 +1,57 @@
+/* =====================header================= */
+
+const elbtn = document.querySelector('button');
+const elNav = document.querySelector('header > .nav');
+const elUl = document.querySelector('header > .nav > ul');
+
+elbtn.addEventListener('click',function(){
+    elbtn.classList.toggle('active');
+    elNav.classList.toggle('active');
+});
+
+/* ====================first==================== */
+
+let num = 0, inter, fadeFun;
+$('.first figure').not(':first').hide();
+fadeFun = function(param){
+    $('.first figure').eq(num).fadeOut(1000);
+    if(param == '처음') {
+        $('.first figure').eq(0).fadeIn(1000);
+    }
+    else if(param == '가운데') {
+        $('.first figure').eq(1).fadeIn(1000);
+    }
+    else {
+        $('.first figure').eq(2).fadeIn(1000);
+    }
+    num++;
+    $('.first figure').eq(num).fadeIn(1000);
+    if(num == 3) {
+        num = 0;
+        $('.first figure').eq(num).fadeIn(1000);
+    }
+}
+
+inter = setInterval(fadeFun,5000,);
+
+$('.fade-btn')
+.on('mouseenter', function(){
+    clearInterval(inter);
+})
+.on('mouseleave', function(){
+    inter = setInterval(fadeFun,3000);
+});
+
+$('.fade-btn button').on('click', function(){
+    let idx = $(this).index();
+    if(idx == 0) {
+        fadeFun('처음');
+    }
+    else if(idx == 1) {
+        fadeFun('가운데')
+    }
+    else {
+        fadeFun('마지막');
+    }
+});
+
