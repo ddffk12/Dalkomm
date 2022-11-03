def4ff72d2b9a4ab6255a14fdd4f2c4bd30aef62
ddffk12 <ddffk12@naver.com>
2022-11-03 16:08:36 +0900

    up

--- javascript/index.js
+++ javascript/index.js
@@ -45,51 +45,23 @@ $('.nav > li').on('click',function(){
 
 /* ====================first==================== */
 
-let num = 0, inter, fadeFun;
-$('.first figure').not(':first').hide();
-fadeFun = function(param){
-    $('.first figure').eq(num).fadeOut(1000);
-    num++;
-    if(param == '처음') {
-        $('.first figure').eq(0).fadeIn(1000);
-    }
-    else if(param == '가운데') {
-        $('.first figure').eq(1).fadeIn(1000);
-    }
-    else if(param == '마지막') {
-        $('.first figure').eq(2).fadeIn(1000);
-    }
-    else if(num == 3) {
-        num = 0;
-        $('.first figure').eq(num).fadeIn(1000);
-    }
-    else {
-        $('.first figure').eq(num).fadeIn(1000);
-    }
-}
 
-inter = setInterval(fadeFun,5000,);
-
-$('.fade-btn')
-.on('mouseenter', function(){
-    clearInterval(inter);
-})
-.on('mouseleave', function(){
-    inter = setInterval(fadeFun,3000);
+var swiper = new Swiper(".fade", {
+    spaceBetween: 30,
+    effect: "fade",
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 });
 
-$('.fade-btn button').on('click', function(){
-    let idx = $(this).index();
-    if(idx == 0) {
-        fadeFun('처음');
-    }
-    else if(idx == 1) {
-        fadeFun('가운데')
-    }
-    else {
-        fadeFun('마지막');
-    }
-});
 /* ===================second================== */
 
 function loading(){
